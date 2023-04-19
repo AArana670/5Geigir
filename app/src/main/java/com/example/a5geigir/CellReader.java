8d3aee8bd81dad61f2d6c5965655ccc9af7f0897
AArana670 <76699832+AArana670@users.noreply.github.com>
2023-04-20 00:24:05 +0200

    Medición de valores reales

--- app/src/main/java/com/example/a5geigir/CellReader.java
+++ app/src/main/java/com/example/a5geigir/CellReader.java
@@ -2,9 +2,35 @@ package com.example.a5geigir;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
+import android.telephony.CellIdentity;
+import android.telephony.CellIdentityCdma;
+import android.telephony.CellIdentityGsm;
+import android.telephony.CellIdentityLte;
+import android.telephony.CellIdentityNr;
+import android.telephony.CellIdentityTdscdma;
+import android.telephony.CellIdentityWcdma;
+import android.telephony.CellInfo;
+import android.telephony.CellInfoCdma;
+import android.telephony.CellInfoGsm;
+import android.telephony.CellInfoLte;
+import android.telephony.CellInfoNr;
+import android.telephony.CellInfoTdscdma;
+import android.telephony.CellInfoWcdma;
+import android.telephony.CellSignalStrength;
+import android.telephony.CellSignalStrengthCdma;
+import android.telephony.CellSignalStrengthGsm;
+import android.telephony.CellSignalStrengthLte;
+import android.telephony.CellSignalStrengthNr;
+import android.telephony.CellSignalStrengthTdscdma;
+import android.telephony.CellSignalStrengthWcdma;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
 import androidx.preference.PreferenceManager;
 import androidx.room.Room;
 
@@ -19,49 +45,192 @@ import java.util.List;
 public class CellReader {
 
     private static CellReader instance = null;
+    private Context context;
     private LocationController locationController;
     private SharedPreferences prefs;
+    private TelephonyManager telephonyManager;
 
-    private CellReader(Context context){
+    private CellReader(Context context) {
+        this.context = context;
         locationController = LocationController.getInstance(context);
-
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
     }
 
-    public static CellReader getInstance(Context context){
-        if(instance == null)
+    public static CellReader getInstance(Context context) {
+        if (instance == null)
             instance = new CellReader(context);
         return instance;
     }
 
-    public List<Signal> readCells(){
-        String moment = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    public List<Signal> readCells() {
 
-        double ubiLat = 0;
-        double ubiLong = 0;
-        if (!prefs.getBoolean("private_mode",false)) {
-            Location ubi = locationController.getLastLocation();
-            ubiLat = ubi.getLatitude();
-            ubiLong = ubi.getLongitude();
+        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.e("Reading", "Trying to read cells without location permission", new Exception());
+            return null;
         }
 
-        int amount = (int) (Math.random()*10)+1;
 
-        List<Signal> readSignals = new ArrayList<Signal>();
+        List<CellInfo> cellList = telephonyManager.getAllCellInfo();
 
-        for (int i = 0; i < amount; i++){
-            int cId = i;
-            int dBm = (int) ((Math.random()*-50)-20);
-            String type = "5G";
-            int freq = (int) ((Math.random()*400)+3400);
-            String provider = "Oscuro";
+        List<Signal> readSignals = new ArrayList<>();
+        Signal currentSignal = null;
 
-            Signal s = new Signal(cId, moment, ubiLat, ubiLong, dBm, type, freq, provider);
+        for (CellInfo c : cellList){
 
-            readSignals.add(s);
+            int idx = cellList.indexOf(c);
+
+            if (c instanceof CellInfoNr){
+                currentSignal = buildSignalNr(c, idx);
+
+            } else if (c instanceof CellInfoLte){
+                currentSignal = buildSignalLte(c, idx);
+
+            } else if (c instanceof CellInfoGsm){
+                currentSignal = buildSignalGsm(c, idx);
+
+            } else if (c instanceof CellInfoWcdma){
+                currentSignal = buildSignalWcdma(c, idx);
+
+            } else if (c instanceof CellInfoTdscdma){
+                currentSignal = buildSignalTdscdma(c, idx);
+            }
+
+            if (currentSignal != null)
+                readSignals.add(currentSignal);
         }
 
         return readSignals;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private Signal buildSignalNr(CellInfo c, int idx) {
+        String moment = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        double ubiLat = 0;
+        double ubiLong = 0;
+        if (!prefs.getBoolean("private_mode", false)) {
+            Location ubi = locationController.getLastLocation();
+            ubiLat = ubi.getLatitude();
+            ubiLong = ubi.getLongitude();
+        }
+
+        CellIdentityNr cId = (CellIdentityNr) c.getCellIdentity();
+        CellSignalStrengthNr cSS = (CellSignalStrengthNr) c.getCellSignalStrength();
+
+        int pCId = cId.getPci();
+        int dBm = cSS.getDbm();
+        String type = "NR";
+        int freq = cId.getNrarfcn();
+        String provider = (String) cId.getOperatorAlphaLong();
+
+        Signal s = new Signal(idx, pCId, moment, ubiLat, ubiLong, dBm, type, freq, provider);
+
+        return s;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private Signal buildSignalLte(CellInfo c, int idx) {
+        String moment = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        double ubiLat = 0;
+        double ubiLong = 0;
+        if (!prefs.getBoolean("private_mode", false)) {
+            Location ubi = locationController.getLastLocation();
+            ubiLat = ubi.getLatitude();
+            ubiLong = ubi.getLongitude();
+        }
+
+        CellIdentityLte cId = (CellIdentityLte) c.getCellIdentity();
+        CellSignalStrengthLte cSS = (CellSignalStrengthLte) c.getCellSignalStrength();
+
+        int pCId = cId.getPci();
+        int dBm = cSS.getDbm();
+        String type = "LTE";
+        int freq = cId.getEarfcn();
+        String provider = (String) cId.getOperatorAlphaLong();
+
+        Signal s = new Signal(idx, pCId, moment, ubiLat, ubiLong, dBm, type, freq, provider);
+
+        return s;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private Signal buildSignalGsm(CellInfo c, int idx) {
+        String moment = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        double ubiLat = 0;
+        double ubiLong = 0;
+        if (!prefs.getBoolean("private_mode", false)) {
+            Location ubi = locationController.getLastLocation();
+            ubiLat = ubi.getLatitude();
+            ubiLong = ubi.getLongitude();
+        }
+
+        CellIdentityGsm cId = (CellIdentityGsm) c.getCellIdentity();
+        CellSignalStrengthGsm cSS = (CellSignalStrengthGsm) c.getCellSignalStrength();
+
+        int pCId = cId.getCid();
+        int dBm = cSS.getDbm();
+        String type = "GSM";
+        int freq = cId.getArfcn();
+        String provider = (String) cId.getOperatorAlphaLong();
+
+        Signal s = new Signal(idx, pCId, moment, ubiLat, ubiLong, dBm, type, freq, provider);
+
+        return s;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private Signal buildSignalWcdma(CellInfo c, int idx) {
+        String moment = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        double ubiLat = 0;
+        double ubiLong = 0;
+        if (!prefs.getBoolean("private_mode", false)) {
+            Location ubi = locationController.getLastLocation();
+            ubiLat = ubi.getLatitude();
+            ubiLong = ubi.getLongitude();
+        }
+
+        CellIdentityWcdma cId = (CellIdentityWcdma) c.getCellIdentity();
+        CellSignalStrengthWcdma cSS = (CellSignalStrengthWcdma) c.getCellSignalStrength();
+
+        int pCId = cId.getCid();
+        int dBm = cSS.getDbm();
+        String type = "WCDMA";
+        int freq = cId.getUarfcn();
+        String provider = (String) cId.getOperatorAlphaLong();
+
+        Signal s = new Signal(idx, pCId, moment, ubiLat, ubiLong, dBm, type, freq, provider);
+
+        return s;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.R)
+    private Signal buildSignalTdscdma(CellInfo c, int idx) {
+        String moment = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Calendar.getInstance().getTime());
+
+        double ubiLat = 0;
+        double ubiLong = 0;
+        if (!prefs.getBoolean("private_mode", false)) {
+            Location ubi = locationController.getLastLocation();
+            ubiLat = ubi.getLatitude();
+            ubiLong = ubi.getLongitude();
+        }
+
+        CellIdentityTdscdma cId = (CellIdentityTdscdma) c.getCellIdentity();
+        CellSignalStrengthTdscdma cSS = (CellSignalStrengthTdscdma) c.getCellSignalStrength();
+
+        int pCId = cId.getCid();
+        int dBm = cSS.getDbm();
+        String type = "TDSCDMA";
+        int freq = cId.getUarfcn();
+        String provider = (String) cId.getOperatorAlphaLong();
+
+        Signal s = new Signal(idx, pCId, moment, ubiLat, ubiLong, dBm, type, freq, provider);
+
+        return s;
+    }
 }
